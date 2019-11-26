714f2e7d3e5e1a18fc486f8f5ea74e8189410ca5
IgTriguero <itrigueropedraza@gmail.com>
2019-11-26 14:08:52 +0100

    Primer commit y medio hecho header en home
    Co-authored-by: Julián de Pablo <38203898+julidpt@users.noreply.github.com>

---
+++ Gymnious/src/app/home/home.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor(private titleService: Title) {}
+
+  ngOnInit(){
+    this.titleService.setTitle("Gymnious")
+  }
+
+}
